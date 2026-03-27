76223faa53c3bbc1361f1f7ff0767145cc8d69dd
Masanari Mizuno <mznms@primu.la>
2026-03-28 08:29:45 +0900

    Revert "using cd instead of z"
    This reverts commit a4f13a3edab607aa2c194b975f1703310794e2b1.

--- init.zsh
+++ init.zsh
@@ -1,4 +1,4 @@
 if (( $+commands[zoxide] )); then
-    $commands[zoxide] init zsh --cmd cd >| ${0:A:h}/zoxide.zsh
+    $commands[zoxide] init zsh >| ${0:A:h}/zoxide.zsh
     source ${0:A:h}/zoxide.zsh
 fi
