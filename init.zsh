7aa363d1859621dd44851380058e683d38e87c7e
Masanari Mizuno <mznms@primu.la>
2026-03-28 11:23:48 +0900

    feat: bind Ctrl+T to zoxide interactive selection (zi)

--- init.zsh
+++ init.zsh
@@ -1,4 +1,12 @@
 if (( $+commands[zoxide] )); then
     $commands[zoxide] init zsh >| ${0:A:h}/zoxide.zsh
     source ${0:A:h}/zoxide.zsh
+
+    # Ctrl+T: launch zoxide interactive selection (zi)
+    function _zoxide-interactive-widget() {
+        zi
+        zle reset-prompt
+    }
+    zle -N _zoxide-interactive-widget
+    bindkey '^T' _zoxide-interactive-widget
 fi
